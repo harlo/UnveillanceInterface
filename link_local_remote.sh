c6fc8f415216c1aa25bf59f8ab9184b1239ea07b
harlo <harlo.holmes@gmail.com>
2014-03-31 13:43:17 -0400

    synctask stubs

--- link_local_remote.sh
+++ link_local_remote.sh
@@ -8,11 +8,14 @@ LOCAL_REMOTE_PWD=$3
 REMOTE_HOST=$4
 REMOTE_PORT=$5
 
-# ssh into annex to establish trust
-ssh -t -i $SSH_ROOT/unveillance.local_remote.key root@$REMOTE_HOST -p $REMOTE_PORT -v 'sudo echo "$LOCAL_REMOTE_PWD"'
+# ssh into annex to establish trust (AKA ToFUPoP)
+ssh -t -oStrictHostKeyChecking=no -i $SSH_ROOT/unveillance.local_remote.key root@$REMOTE_HOST -p $REMOTE_PORT -v 'sudo echo "$LOCAL_REMOTE_PWD"'
 
 # append to ssh/config
 echo unveillance.local_remote.port: $REMOTE_PORT >> $LOCAL_CONFIG
+# Host $REMOTE_HOST
+#	IdentityFile $SSH_ROOT/unveillance.local_remote.key
+#	Port $REMOTE_PORT
 
 git clone ssh://root@$REMOTE_HOST/home/unveillance_remote $LOCAL_REMOTE_FOLDER
 cd $LOCAL_REMOTE_FOLDER
