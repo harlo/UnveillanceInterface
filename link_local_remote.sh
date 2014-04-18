06e30397c1ed027cf75be5958bf298f30e28641d
harlo <harlo.holmes@gmail.com>
2014-04-17 23:47:45 -0400

    updates

--- link_local_remote.sh
+++ link_local_remote.sh
@@ -5,10 +5,12 @@ LOCAL_CONFIG=$OLD_DIR/conf/local.config.yaml
 SSH_ROOT=$1
 LOCAL_REMOTE_FOLDER=$2
 REMOTE_HOST=$3
-REMOTE_PORT=$4
+REMOTE_USER=$4
+REMOTE_PATH=$5
+REMOTE_PORT=$6
 
 # ssh into annex to establish trust (AKA ToFUPoP)
-ssh -t -oStrictHostKeyChecking=no -i $SSH_ROOT/unveillance.local_remote.key root@$REMOTE_HOST -p $REMOTE_PORT -v <<EOF
+ssh -t -oStrictHostKeyChecking=no -i $SSH_ROOT/unveillance.local_remote.key $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT -v <<EOF
 echo "hello" > /dev/null 2>&1
 EOF
 
@@ -18,7 +20,7 @@ echo "Host $REMOTE_HOST" >> $SSH_ROOT/config
 echo "	IdentityFile $SSH_ROOT/unveillance.local_remote.key" >> $SSH_ROOT/config
 echo "	Port $REMOTE_PORT" >> $SSH_ROOT/config
 
-git clone ssh://root@$REMOTE_HOST/home/unveillance_remote $LOCAL_REMOTE_FOLDER
+git clone ssh://$REMOTE_USER@$REMOTE_HOST$REMOTE_PATH $LOCAL_REMOTE_FOLDER
 cd $LOCAL_REMOTE_FOLDER
 mkdir .synctasks
 echo .DS_Store > .gitignore
@@ -28,7 +30,7 @@ echo .synctasks/local/ >> .gitignore
 
 git annex init "unveillance_remote"
 git annex untrust web
-git remote add unveillance_remote ssh://root@$REMOTE_HOST/home/unveillance_remote
+git remote add unveillance_remote ssh://$REMOTE_USER@$REMOTE_HOST$REMOTE_PATH
 
 cd $OLD_DIR
 echo True
