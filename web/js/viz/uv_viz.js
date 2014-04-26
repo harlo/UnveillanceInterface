493a6879cbcf84204702f173eed6bff516ebe191
harlo <harlo.holmes@gmail.com>
2014-04-25 21:20:03 -0400

    d3 stuff

--- web/js/viz/uv_viz.js
+++ web/js/viz/uv_viz.js
@@ -54,7 +54,12 @@ var UnveillanceViz = Backbone.Model.extend({
 });
 
 function getRandomColor() {
-	return "#cccccc";
+	var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 function drillToKey(obj, key) {
