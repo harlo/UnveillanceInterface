cd6fbef124cca48ce6b88c0bc909347f540d5f13
harlo <harlo.holmes@gmail.com>
2014-05-21 12:58:04 -0400

    fab stuff

--- Utils/fab_api.py
+++ Utils/fab_api.py
@@ -5,14 +5,22 @@ from fabric.api import *
 from conf import DEBUG, getConfig
 
 uv = 'unveillance.local_remote'
-env.hosts = [getConfig('%s.hostname', uv)]
+
+uv_user = getConfig('%s.user' % uv)
+remote_path = getConfig('%s.remote_path' % uv)
+hostname = getConfig('%s.hostname' % uv)
+port = getConfig('%s.port' % uv)
+port_prefix = ""
+
+if port != 22:
+	port_prefix += ":%d" % port
+
+env.hosts = ["%s@%s%s" % (uv_user, hostname, port_prefix)]
 
 def linkLocalRemote():
 	if DEBUG: print "linking local remote"
 	
 	folder = getConfig('%s.folder' % uv)
-	remote_path = getConfig('%s.remote_path' % uv)
-	uv_user = getConfig('%s.user' % uv)
 
 	local("git clone ssh://%s@%s%s %s" % (uv_user, env.hosts[0], remote_path, folder))
 	local("cd %s" % folder)
@@ -29,7 +37,15 @@ def linkLocalRemote():
 	
 	return True
 
-def netcat():
+def netcat(file=None):
 	if DEBUG: print "OH HEY NETCAT"
+	if file is None: return False
+	
+	from conf import ANNEX_DIR
+	with cd(ANNEX_DIR):
+		put(file, os.path.join(remote_path, file))
+		run("cd %s" % remote_path)
+		run("git annex sync")
+		return True
 	
 	return False
