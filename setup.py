638007d6483cda55984513dc373873432b9d60f8
harlo <harlo.holmes@gmail.com>
2014-07-16 10:15:00 -0400

    rewriting config

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ if __name__ == "__main__":
 		local("mkdir %s" % os.path.join(base_dir, ".users"))
 	
 	with settings(hide('everything'), warn_only=True):
-		local("rm %s" % os.path.join(base_dir, "conf", "unveillance.secrets.json")
+		local("rm %s" % os.path.join(base_dir, "conf", "unveillance.secrets.json"))
 	
 	secrets_config = os.path.join(base_dir, "conf", "unveillance.secrets.json")		
 	with open(secrets_config, "wb+") as CONFIG:
