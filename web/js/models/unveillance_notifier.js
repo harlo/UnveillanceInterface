e066ac85827ad79c850aaec04c94250b73b9e849
harlo <harlo.holmes@gmail.com>
2015-02-27 18:25:40 +0000

    annex channel fix

--- web/js/models/unveillance_notifier.js
+++ web/js/models/unveillance_notifier.js
@@ -2,10 +2,15 @@ var UnveillanceNotifier = Backbone.Model.extend({
 	constructor: function() {
 		Backbone.Model.apply(this, arguments);
 
-		var web_socket = new SockJS(
-			(!UV.TASK_CHANNEL_URL.match(/127\.0\.0\.1|localhost/) ? UV.TASK_CHANNEL_URL : UV.TASK_CHANNEL_URL.replace(/127\.0\.0\.1|localhost/gi, window.location.host.split(':')[0])) 
-				+ "/annex_channel",
-			null, { protocols_whitelist : ['websocket']});
+		var task_channel = UV.TASK_CHANNEL_URL;
+		
+		if(UV.TASK_CHANNEL_URL.match(/127\.0\.0\.1|localhost/)) {
+			task_channel = UV.TASK_CHANNEL_URL.replace(/127\.0\.0\.1|localhost/gi, window.location.host).split(':');
+			task_channel.pop();
+			task_channel = task_channel.join(':');
+		}
+
+		var web_socket = new SockJS(task_channel + "/annex_channel", null, { protocols_whitelist : ['websocket']});
 
 		web_socket.onopen = this.onSocketOpen;
 		web_socket.onclose = this.onSocketClose;
